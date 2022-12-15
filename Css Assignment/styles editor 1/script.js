1d0c69004128fb21a5ffef2dbd465cf1d1e5f27c
qamarali205 <qamarali205@gmail.com.com>
2022-12-15 11:20:14 +0530

    CSS

---
+++ Css Assignment/styles editor 1/script.js
@@ -0,0 +1,7 @@
+// javascript code goes here
+function onSubmit(padding,margin){
+
+    document.getElementById("main").style.padding= padding +"px" ;
+    document.getElementById('main').style.margin = margin +"px";
+ 
+}
---
+++ Css Assignment/styles editor 2/script.js
@@ -0,0 +1,8 @@
+// javascript code goes here
+
+function onSubmit(left,top){
+
+    document.getElementById("main").style.position="absolute";
+    document.getElementById('main').style.top = top + "px";
+    document.getElementById('main').style.left = left + "px";
+}
